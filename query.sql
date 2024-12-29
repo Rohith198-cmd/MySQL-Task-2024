ab94bc3db7683a9b95c1d55d33d9e91482c3ac92
Rohith198-cmd <rohithpalani67@gmail.com>
2024-12-29 14:01:19 +0530

    task-4completed

--- query.sql
+++ query.sql
@@ -1,1 +1,14 @@
 -- Task Work
+CREATE TABLE emp(
+ employee_id INT PRIMARY KEY,
+ empname VARCHAR(50),
+ dep VARCHAR(50),
+ salary BIGINT
+ );
+ ALTER TABLE emp ADD COLUMN position VARCHAR(50) NOT NULL;
+ ALTER TABLE emp MODIFY salary INT UNSIGNED;
+ ALTER TABLE emp RENAME COLUMN dep TO department;
+ INSERT INTO emp VALUES (1,'walter','manager',-1,50000);
+ ALTER TABLE emp DROP COLUMN position;
+ 
+ SELECT * FROM emp;
